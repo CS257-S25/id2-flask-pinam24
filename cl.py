1496666686ae3856dc6c0cf1b6a35e3f329d5042
pinam24 <pinam@carleton.edu>
2025-04-25 11:01:33 -0500

    removed search functions in cl.py for style check

--- cl.py
+++ cl.py
@@ -5,7 +5,6 @@ This will be the entry point for the project when run from the command line.
 
 import sys
 import argparse
-from ProductionCode.search import search_title, search_author, search_genre
 from ProductionCode.most_banned import (
     most_banned_districts,
     most_banned_authors,
@@ -22,33 +21,6 @@ def main():
         description="Command line interface for the project",
         epilog="This is the command line interface for the project.",
         usage="%(prog)s [options] [args]",
-    )
-    parser.add_argument(
-        # Search for a title in the database
-        "--search-title",
-        "--st",
-        "-st",
-        help="Search for a title in the database",
-        type=str,
-        metavar="TITLE",
-    )
-    parser.add_argument(
-        # Search for an author in the database
-        "--search-author",
-        "--sa",
-        "-sa",
-        help="Search for an author in the database",
-        type=str,
-        metavar="AUTHOR",
-    )
-    parser.add_argument(
-        # Search for a genre in the database
-        "--search-genre",
-        "--sg",
-        "-sg",
-        help="Search for a genre in the database",
-        type=str,
-        metavar="GENRE",
     )
     parser.add_argument(
         # Get the most banned districts in the database
@@ -88,9 +60,6 @@ def main():
     )
     args = parser.parse_args()
     dispatch = {
-        "search_title": search_title,
-        "search_author": search_author,
-        "search_genre": search_genre,
         "most_banned_districts": most_banned_districts,
         "most_banned_authors": most_banned_authors,
         "most_banned_states": most_banned_states,
